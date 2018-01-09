275e47db1571ac00356a10ba48cb0ebfa4ee2ea1
Fernando Garcia <agarcia@vates.org>
2018-01-08 21:55:30 -0300

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Conexión a la base de datos
 mongoose.connection.openUri(
@@ -16,12 +26,9 @@ mongoose.connection.openUri(
 );
 
 // Rutas
-app.get('/', (req, res, next) => {
-  res.status(200).json({
-    ok: true,
-    mensaje: 'Peticion realizada correctamente'
-  });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 // Escuchar peticiones
 app.listen(3000, () => {
